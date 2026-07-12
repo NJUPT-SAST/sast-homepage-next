fc5715a4d91fdc91c6a36ada083f065c406a8176
SeanDictionary <sean.dictionary@qq.com>
2026-07-12 22:07:14 +0800

    feat: footer add bilibili & qrcode

--- components/layout/site-footer/index.tsx
+++ components/layout/site-footer/index.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
 import styles from "./site-footer.module.css";
 
+const footerQrItems = [
+  {
+    src: "/share/qr/qq-qrcode.png",
+    alt: "SAST 官方QQ",
+    remark: "SAST 官方QQ",
+  },
+  {
+    src: "/share/qr/sast-26-qrcode.png",
+    alt: "SAST 2026招新群",
+    remark: "SAST 2026招新群",
+  },
+];
+
 export default function SiteFooter() {
   return (
     <footer className={styles.footer}>
@@ -9,8 +22,12 @@ export default function SiteFooter() {
         <div className={styles.footerDetail}>
           <p>
             <Image src="/share/icons/contact/mail.png" alt="" width={15} height={15} className={styles.footerIcon} />
-            <a href="mailto:njuptsast@163.com">
-              njuptsast@163.com
+            <a href="mailto:njuptsast@163.com">njuptsast@163.com</a>
+          </p>
+          <p>
+            <Image src="/share/icons/contact/bilibili.png" alt="" width={15} height={15} className={styles.footerIcon} />
+            <a href="https://space.bilibili.com/385170291" target="_blank" rel="noopener noreferrer">
+              SAST B站官方账号
             </a>
           </p>
           <p>
@@ -20,8 +37,14 @@ export default function SiteFooter() {
         </div>
         <div className={styles.footerDivider} />
         <div className={styles.footerQr}>
-          <Image src="/share/qr/qq-qrcode.png" alt="QQ 群二维码" width={104} height={104} className={styles.qrImage} />
-          <p className={styles.qrRemark}>SAST 官方QQ</p>
+          <div className={styles.footerQrList}>
+            {footerQrItems.map((item, index) => (
+              <div key={`${item.src}-${index}`} className={styles.qrItem}>
+                <Image src={item.src} alt={item.alt} width={104} height={104} className={styles.qrImage} />
+                <p className={styles.qrRemark}>{item.remark}</p>
+              </div>
+            ))}
+          </div>
         </div>
         <p className={styles.copyright}>Copyright © NJUPT.SAST 1992-2026</p>
       </div>
