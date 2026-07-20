3e465a5dd36f8c49b1df2541f6c66114da5d3adf
SeanDictionary <sean.dictionary@qq.com>
2026-07-20 22:13:45 +0800

    feat: add web icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,12 @@ import { Dock, SiteFooter } from "@/components/layout";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SAST",
+  title: "SAST-南京邮电大学大学生科学技术协会",
   description: "南京邮电大学大学生科学技术协会",
+  icons: [
+    { url: "/logo-black-square.ico", media: "(prefers-color-scheme: light)" },
+    { url: "/logo-white-square.ico", media: "(prefers-color-scheme: dark)" },
+  ],
 };
 
 export default function RootLayout({
